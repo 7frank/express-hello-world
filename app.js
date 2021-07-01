6274994aebdc6f2183c48a9525dd520ae148d48c
7frank <frank.next@gmx.de>
2021-07-01 13:54:06 +0000

    experimenting with gitpod

--- app.js
+++ app.js
@@ -4,4 +4,31 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.send("Hello there!"));
 
+
+app.get("/!", (req, res) => res.send("Hello there!"));
+
+app.get("/routes", (req, res) => res.send(getRoutes(app)));
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+function getRoutes(app)
+{
+
+    var route, routes = [];
+
+    app._router.stack.forEach(function(middleware){
+        if(middleware.route){ // routes registered directly on the app
+            routes.push(middleware.route);
+        } else if(middleware.name === 'router'){ // router middleware 
+            middleware.handle.stack.forEach(function(handler){
+                route = handler.route;
+                route && routes.push(route);
+            });
+        }
+    });
+
+    return routes
+}
